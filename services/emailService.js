82c0a537d7ef40151d561138af8441d5f671dad0
Rohanaow123 <Rohan@aow.co.in>
2025-08-26 14:45:57 +0530

    update

--- services/emailService.js
+++ services/emailService.js
@@ -4,6 +4,7 @@ class EmailService {
   constructor() {
     this.transporter = nodemailer.createTransport({
       service: "gmail",
+      secure: "false",
       auth: {
         user: process.env.EMAIL_USER || "freshorappc@gmail.com",
         pass: process.env.GMAIL_APP_PASSWORD || "ykbl euac vysy dpta", // Gmail App Password
